a37d84b58ec813df074504f28ae61adc45c94fed
manojkrajasekar <manoj.k.rajasekar@gmail.com>
2018-02-20 00:14:42 -0500

    Initial commit

---
+++ Vidjot/routes/users.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+//Local User Model
+require('../models/Users');
+const User = mongoose.model('users');
+
+//Users login Route
+router.get('/login', (req,res) => {
+   res.render('./users/login');
+});
+
+router.get('/register', (req,res) => {
+    res.render('./users/register');
+});
+
+router.post('/login', (req,res,next) => {
+    passport.authenticate('local', {
+        successRedirect:'/ideas',
+        failureRedirect:'/users/login',
+        failureFlash: true 
+    })(req, res, next);
+});
+
+router.post('/register', (req,res)=> {
+    let errors = [];
+    if(req.body.password != req.body.password2 )
+    {
+        errors.push({text:'Passwords do not match'});
+    }
+    
+    if(req.body.password.length < 4)
+    {
+        errors.push({text: 'password must be atleast 4 characters'});
+    }
+
+    if(errors.length >0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passowrd2: req.body.passowrd2
+        });
+    } 
+    else{
+        User.findOne({email: req.body.email, name:req.body.name})
+            .then(user => {
+                if(user){
+                    req.flash('error_msg', 'Email already regsitered');
+                    res.redirect('/users/register');
+                }else {
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    })
+            
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg', 'you have registered successfully');
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                    return;
+                                });
+                        });
+                    });
+                    console.log(newUser);
+                }
+            })
+        }
+});
+
+
+router.get('/logout', (req, res) => {
+    req.logOut();
+    req.flash('success_msg', 'you are logged out');
+    res.redirect('/users/login');
+});
+
+module.exports = router;
